64e11282e5a025a981af24722bef541fbac65c9c
rajashekar reddy <chrajashekarreddy777@gmail.com>
2021-12-15 12:04:27 +0530

    adding the employeeWage of uc 7 into remote repo

---
+++ com/bl/Employeewageuc7/Practise/problems/EmployeeWageUC7.java
@@ -0,0 +1,41 @@
+package com.bl.Employeewageuc7.Practise.problems;
+
+public class EmployeeWageUC7 {
+	static final int IS_PRESENT = 1;
+	static final int IS_PART_TIME = 2;
+	private static final int FULL_DAY_WORKING_HOUR = 0;
+	public static void employeewage(int SALARY_PER_HOUR, int TOTAL_WORK_DAY, int MAX_WORKING_HOUR, int FULL_DAY_WORKING_DAY ) {
+		int empCheck  = 0;
+		int empHrs = 0;
+		int totalSalary = 0;
+		int day = 0;
+		while(day<TOTAL_WORK_DAY && (empHrs+FULL_DAY_WORKING_HOUR) <= MAX_WORKING_HOUR) {
+			day++;
+			empCheck= (int)(Math.floor(Math.random()*10) % 3);
+			switch (empCheck) {
+			case IS_PRESENT:
+				empHrs+=FULL_DAY_WORKING_HOUR;
+				break;
+			case IS_PART_TIME:
+				empHrs+=(FULL_DAY_WORKING_HOUR/2);
+				break;
+			default:
+				empHrs+=0;
+				break;
+			}
+			System.out.println("working day :"+day+"(Present : "+empCheck+")");
+			
+		}
+		int SALARI_PER_HOUR = 0;
+		totalSalary = empHrs*SALARI_PER_HOUR;
+		System.out.println("Monthly salary of emp : "+totalSalary+"\nTotalworking :"+empHrs);
+	}
+	public static void main(String[] args) {
+		employeewage(20, 20, 100, 8);
+		
+	}
+
+	
+	
+
+}
